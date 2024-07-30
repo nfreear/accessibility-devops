7b5b1fdf66bf0516cd2d4cc3490d5d172ca02e59
Nick Freear <nfreear@yahoo.co.uk>
2024-07-30 21:36:59 +0100

    fix: Add `.pa11yci.js` Pa11y-ci Configuration

---
+++ .pa11yci.js
@@ -0,0 +1,25 @@
+/**
+ * Pa11y-ci configuration.
+ *
+ * @copyright © Nick Freear, 30-July-2024.
+ * @see https://opensource.com/article/23/2/automated-accessibility-testing
+ * @see https://gitlab.com/civicactions/accessibility
+ */
+
+const PORT = 8080;
+const executablePath = process.env.CI ? '/usr/bin/google-chrome' : null;
+
+module.exports = {
+  defaults: {
+    standard: 'WCAG2AA',
+    chromeLaunchConfig: {
+      executablePath,
+      args: ['--disable-dev-shm-usage', '--no-sandbox', '--disable-gpu']
+    }
+  },
+
+  urls: [
+    `http://127.0.0.1:${PORT}/pass.html`,
+    `http://127.0.0.1:${PORT}/fail.html`
+  ]
+};
